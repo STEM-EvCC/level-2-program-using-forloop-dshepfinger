188f74a826263c7335921c113f1da273f270cc72
dshepfinger <dshepfinger@students.everettcc.edu>
2025-03-01 19:33:23 +0000

    Beginning of code

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
+# mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
+# mission_success = [True, False, True, True, True, True, False]
+
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
-mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
-mission_success = [True, False, True, True, True, True, False]
+for i in range(len(mission_names)):
+        print(f"- {i} {mission_names[i]}")
