c13bbaf5b64248957bbc5ba10f7f9931c9a5397a
dshepfinger <dshepfinger@students.everettcc.edu>
2025-03-01 21:24:40 +0000

    2

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
-# mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
-# mission_success = [True, False, True, True, True, True, False]
-
+# Information provided already
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
-for i in range(len(mission_names)):
-        print(f"- {i} {mission_names[i]}")
+mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
+mission_success = [True, False, True, True, True, True, False]
+
+# Counters for number of missions, before 2000, and their success
+missions_total = 0
+
+missions_before_2000 = []
+
+missions_wereSuccesful = 0
+
